31b7aac37db26b4b4b697a596fca22c3396552b1
myroman-wuLtCL <myroman@protonmail.com>
2023-04-12 10:06:24 -0400

    updated packages

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is your github username?),
+    name: 'username',
+},
+    type: 'input',
+    message: 'What is the title of your repository?',
+    name: 'title',
+},
+type: 'input',
+    message: 'What is your email address?',
+        name: 'email',
+},
+type: 'choice',
+    message: 'Please choose a license',
+        name: 'license',
+},
+])
+.then(function (response) {
+            console.log(response);
+            const stringResponse = JSON.stringify(response)
+
+        }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
