727ae7a80be7c17a748ced5746cccc8e56918117
myroman-wuLtCL <myroman@protonmail.com>
2023-04-12 19:47:20 -0400

    updated genMD

--- index.js
+++ index.js
@@ -2,44 +2,60 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-const util = require('util');
+//const util = require('util');
 
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt = ([{
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'username',
-    },
-    {
-        type: 'input',
-        message: 'What is the title of your repository?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
-    {
-        type: 'choice',
-        message: 'Please choose a license',
-        name: 'license',
-    },
-    ])
-        .then(function (response) 
-            console.log(response);
-            const stringResponse = JSON.stringify(response)
 
-            fs.writeFile(log.txt)
 
-        
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: 'What is the title of your repository?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+            },
+            {
+                type: 'list',
+                message: 'Please choose a license',
+                name: 'license',
+                choices: ["MIT", "GNU", "OpenBSD", "None"]
+            },
+        ])
+        .then(function (response)  {
+
+            console.log("User Collected data: ", response);
+
+
+            let result = generateMarkdown(response);
+
+            console.log("Returned Data: ", result);
+
+
+            const stringResponse = JSON.stringify(response)
+
+            //fs.writeFile(log.txt)
+        })
+        .catch(function(error) {
+            throw error;
+        }) 
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (badge == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+  }
+  if (badge == "GNU") {
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]"
+  }
+  if (badge == "OpenBSD") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]"
+  }
+  if (license == "None")
+    return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // What do we return from this funtion?
+  if (license == "MIT") {
+    return "(https://opensource.org/licenses/MIT)"
+  }
+  if (license == "GNU") {
+    return "(https://www.gnu.org/licenses/gpl-3.0)"
+  }
+  if (license == "OpenBSD") {
+    return "(https://opensource.org/licenses/BSD-3-Clause)"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "None") {
+    return ""
+  }
+  return "# License";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+# ${data.username}
+
+${renderLicenseSection(data.license)}
+
 
 `;
 }
